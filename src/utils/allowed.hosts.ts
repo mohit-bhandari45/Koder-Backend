394173a4f838fa549f07d45ee3d2bb6355f2f6e9
mohit-bhandari45 <mohitbhandari852@gmail.com>
2025-08-07 10:26:38 +0530

    cors fix

--- src/utils/allowed.hosts.ts
+++ src/utils/allowed.hosts.ts
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
     "http://localhost:3000",
-    "https://your-frontend-domain.com", // Replace with your real deployed frontend URL
+    "https://specified-peacock-mohit123-1b61bd1c.koyeb.app", // Replace with your real deployed frontend URL
 ];
